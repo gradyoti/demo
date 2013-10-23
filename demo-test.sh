d04f839bec10ca6693fc91d98f62f56b53c50322
Josh King <jking@chambana.net>
2013-10-23 11:26:29 -0400

    Add testing script.

---
+++ demo-test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: demo-test.sh
+# 
+#         USAGE: ./demo-test.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (), 
+#  ORGANIZATION: 
+#       CREATED: 10/23/2013 10:57
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+STATUS=0
+
+echo "Testing addition."
+[ $(./demo.sh -a 2 2) != 4 ] && echo "FAILED!"; STATUS=1
+
+[[ $STATUS == 0 ]] && echo "PASSED!"
+exit $STATUS
